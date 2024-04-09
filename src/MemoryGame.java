4dc5750008378615852b00df0836638f25b89e81
Yunzhu (Jessica) Chen <jchen4@macalester.edu>
2024-04-09 10:31:48 -0500

    Initial Setup

---
+++ src/MemoryGame.java
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ */
+public class MemoryGame {
+    private static UI ui;
+    private static CharacterManager manager;
+
+    public static void main(String[] args) {
+        ui = new UI();
+        manager = new CharacterManager();
+    }
+}
---
+++ src/UI.java
@@ -0,0 +1,20 @@
+import edu.macalester.graphics.CanvasWindow;
+
+/**
+ * 
+ */
+public class UI {
+    private static CanvasWindow window;
+    private final static int WINDOW_HEIGHT = 800;
+    private final static int WINDOW_WIDTH = 1500;
+
+    /**
+     * 
+     */
+    public UI(){
+        window = new CanvasWindow("Memory Game", WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+
+
+
+}
