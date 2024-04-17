19e3407108d16cdd343a5cef76979c983500610a
Yunzhu (Jessica) Chen <jchen4@macalester.edu>
2024-04-17 10:02:58 -0500

    Added Path directory

--- src/CharacterPNGPath.java
+++ src/CharacterPNGPath.java
@@ -1,5 +1,30 @@
-public class CharacterPNGPath {
-    
+import java.util.HashMap;
 
-    
+public class CharacterPNGPath{
+    public static HashMap<String, String> characterPath = new HashMap<String, String>();
+
+    /**
+     * 
+     */
+    public CharacterPNGPath(){
+        characterPath.put("Black Panther", "res/black.panther.png");
+        characterPath.put("Black Widow", "res/black.widow.png");
+        characterPath.put("Captain America", "res/captain.america.png");
+        characterPath.put("Hawkeye", "res/hawkeye.png");
+        characterPath.put("Hulk", "res/hulk.png");
+        characterPath.put("Iron Man", "res/iron.man.png");
+        characterPath.put("Scarlet Witch", "res/scarlet.witch.png");
+        characterPath.put("Star Lord", "res/star.lord.png");
+        characterPath.put("Thor", "res/thor.png");
+        characterPath.put("Vision", "res/vision.png");
+    }
+
+    /**
+     * 
+     * @param name
+     * @return
+     */
+    public String get(String name){
+        return characterPath.get(name);
+    }
 }
