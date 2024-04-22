9c3f6ffbfa5b786dcda552ccbec382a74afa9268
Yunzhu (Jessica) Chen <jchen4@macalester.edu>
2024-04-22 10:11:18 -0500

    Added GetSequence method

--- test/PlaceCharacterTest.java
+++ test/PlaceCharacterTest.java
@@ -6,8 +6,6 @@ import edu.macalester.graphics.CanvasWindow;
 
 public class PlaceCharacterTest {
 
-    CharacterManager
-
     public static void main(String[] args) {
         CanvasWindow window = new CanvasWindow("Character Placement Test", 800, 800);
         CharacterManager manager = new CharacterManager(window);
