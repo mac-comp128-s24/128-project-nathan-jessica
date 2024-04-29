eea4f5c975c2a9ef40f8e544506df6ae6f0f93d1
Yunzhu (Jessica) Chen <jchen4@macalester.edu>
2024-04-28 22:27:12 -0500

    added levels and more characters

--- src/CharacterPNGPath.java
+++ src/CharacterPNGPath.java
@@ -18,6 +18,17 @@ public class CharacterPNGPath{
         characterPath.put("Thor", "thor.png");
         characterPath.put("Vision", "vision.png");
         characterPath.put("Baby Groot", "baby_groot.png");
+        characterPath.put("Quick Silver", "quick_silver.png");
+        characterPath.put("Falcon",  "falcon.png");
+        characterPath.put("Doctor Strange", "doctor_strange.png");
+        characterPath.put("Drax", "drax.png");
+        characterPath.put("Gamora", "gamora.png");
+        characterPath.put("Mantis", "mantis.png");
+        characterPath.put("Okoye", "okoye.png");
+        characterPath.put("Rocket", "rocket.png");
+        characterPath.put("Spiderman", "spiderman.png");
+        characterPath.put("Valkyrie", "valkyrie.png");
+        characterPath.put("War Machine", "war_machine.png");
     }
 
     /**
--- src/Levels.java
+++ src/Levels.java
@@ -7,14 +7,24 @@ public class Levels {
     private static HashMap<Integer, String[]> levels = new HashMap<Integer, String[]>();
     private static String[] level1 = 
         {"Black Widow", "Captain America", "Hawkeye", "Hulk", "Iron Man", "Thor"};
+    private static String[] level2 = 
+        {"Black Widow", "Captain America", "Hawkeye", "Hulk", "Iron Man", "Thor", 
+        "Vision", "Wanda", "Quick Silver", "Falcon", "War Machine"};
+    private static String[] level3 = 
+        {"Black Widow", "Captain America", "Hawkeye", "Hulk", "Iron Man", "Thor", 
+        "Vision", "Wanda", "Falcon", "Black Panther", "Baby Groot", "Star Lord", 
+        "Rocket", "Drax", "Spiderman", "Valkyrie", "Mantis", "Doctor Strange", 
+        "Okoye", "War Machine", "Gamora"};
     private static int[] livesLeft = {3, 2, 1};
-    private static double[] initialSpeed = {1, 0.75, 0.25};
+    private static double[] initialSpeed = {1, 0.9, 0.8};
     
     /**
      * 
      */
     public Levels(){
         levels.put(1, level1);
+        levels.put(2, level2);
+        levels.put(3, level3);
     }
 
     
